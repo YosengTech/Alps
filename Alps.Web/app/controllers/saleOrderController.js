3917f397e7c45d391d11b061aa795c476cccc0c4
yosengtech <yosengtech@qq.com>
2015-11-28 21:57:23 +0800

    11.28

---
+++ Alps.Web/app/controllers/saleOrderController.js
@@ -0,0 +1,151 @@
+var Alps;
+(function (Alps) {
+    var Controllers;
+    (function (Controllers) {
+        'use strict';
+        var SaleOrder = (function () {
+            function SaleOrder() {
+            }
+            return SaleOrder;
+        })();
+        Controllers.SaleOrder = SaleOrder;
+        var SaleOrderListCtrl = (function () {
+            function SaleOrderListCtrl(scope, http, toaster) {
+                function getSaleOrderList() {
+                    http.get("/api/SaleOrder").success(function (data) {
+                        scope.items = data;
+                        for (var i = 0; i < scope.items.length; i++) {
+                            scope.items[i].OrderTime = new Date(data[i].OrderTime);
+                        }
+                        toaster.success("提示", "载入成功");
+                    }).error(function (err) {
+                        toaster.error("错误", Alps.phaseErr(err));
+                    });
+                }
+                ;
+                scope.getSaleOrderList = getSaleOrderList;
+                getSaleOrderList();
+            }
+            SaleOrderListCtrl.$inject = ["$scope", "$http", "toaster"];
+            return SaleOrderListCtrl;
+        })();
+        Controllers.SaleOrderListCtrl = SaleOrderListCtrl;
+        var SaleOrderCreateCtrl = (function () {
+            function SaleOrderCreateCtrl(scope, http, toaster, locationService) {
+                function getCustomerIDSelectList() {
+                    http.get("/api/TradeAccount").success(function (data) {
+                        scope.CustomerIDSelectList = data;
+                    }).error(function (err) {
+                        toaster.error("错误", Alps.phaseErr(err));
+                    });
+                }
+                function createSaleOrder() {
+                    http.post("/api/SaleOrder", scope.item).success(function (data) {
+                        toaster.success("提示", "创建成功");
+                        locationService.path("/SaleOrder");
+                    }).error(function (err) {
+                        toaster.pop("error", "错误", Alps.phaseErr(err), 3000, "trustedHtml");
+                    });
+                }
+                ;
+                function goBack() {
+                    window.history.back();
+                }
+                scope.createSaleOrder = createSaleOrder;
+                scope.goBack = goBack;
+                getCustomerIDSelectList();
+            }
+            SaleOrderCreateCtrl.$inject = ["$scope", "$http", "toaster", "$location"];
+            return SaleOrderCreateCtrl;
+        })();
+        Controllers.SaleOrderCreateCtrl = SaleOrderCreateCtrl;
+        var SaleOrderEditCtrl = (function () {
+            function SaleOrderEditCtrl(scope, http, toaster, locationService, routeParams, window) {
+                function getCustomerIDSelectList() {
+                    http.get("/api/TradeAccount").success(function (data) {
+                        scope.CustomerIDSelectList = data;
+                    }).error(function (err) {
+                        toaster.error("错误", err.Message);
+                    });
+                }
+                function getSaleOrder(id) {
+                    http.get("/api/SaleOrder/" + id).success(function (data) {
+                        scope.item = data;
+                        scope.item.OrderTime = new Date(data.OrderTime);
+                        toaster.success("提示", "载入成功");
+                    }).error(function (data) {
+                        toaster.error("错误", data.Message);
+                    });
+                }
+                ;
+                function saveSaleOrder() {
+                    http.put("/api/SaleOrder/" + id, scope.item).success(function (data) {
+                        toaster.success("提示", "保存成功");
+                        locationService.path("/SaleOrder");
+                    }).error(function (err) {
+                        toaster.pop("error", "错误", Alps.phaseErr(err), 3000, "trustedHtml");
+                    });
+                }
+                function submitSaleOrder() {
+                    if (scope.form.$dirty) {
+                        alert("更改未保存");
+                        return;
+                    }
+                    http.post("/api/SaleOrder/" + id + "/submit", {}).success(function (data) {
+                        toaster.success("提示", "保存成功");
+                        locationService.path("/SaleOrder");
+                    }).error(function (err) {
+                        toaster.pop("error", "错误", Alps.phaseErr(err), 3000, "trustedHtml");
+                    });
+                }
+                function deleteSaleOrder() {
+                    if (confirm("确认要删除?")) {
+                        http.delete("/api/SaleOrder/" + id).success(function (data) {
+                            toaster.success("提示", "删除成功");
+                            locationService.path("/SaleOrder");
+                        }).error(function (err) {
+                            toaster.error("错误", Alps.phaseErr(err));
+                        });
+                    }
+                }
+                function goBack() {
+                    window.history.back();
+                }
+                function addSubItem() {
+                    scope.item.Items.push({ "SaleOrderID": scope.item.ID });
+                }
+                function deleteSubItem($index) {
+                    scope.item.Items.splice($index, 1);
+                }
+                function getGoodsIDSelectList() {
+                    http.get("/api/Material").success(function (data) {
+                        scope.GoodsIDSelectList = data;
+                    }).error(function (err) {
+                        toaster.error("错误", err.Message);
+                    });
+                }
+                scope.addSubItem = addSubItem;
+                scope.deleteSubItem = deleteSubItem;
+                scope.getSaleOrder = getSaleOrder;
+                scope.saveSaleOrder = saveSaleOrder;
+                scope.deleteSaleOrder = deleteSaleOrder;
+                scope.submitSaleOrder = submitSaleOrder;
+                scope.goBack = goBack;
+                var id = "";
+                if (routeParams["id"]) {
+                    id = routeParams["id"];
+                    getCustomerIDSelectList();
+                    getGoodsIDSelectList();
+                    getSaleOrder(id);
+                }
+                else {
+                    locationService.path("/SaleOrder");
+                }
+            }
+            SaleOrderEditCtrl.$inject = ["$scope", "$http", "toaster", "$location", "$routeParams", "$window"];
+            return SaleOrderEditCtrl;
+        })();
+        Controllers.SaleOrderEditCtrl = SaleOrderEditCtrl;
+    })(Controllers = Alps.Controllers || (Alps.Controllers = {}));
+})(Alps || (Alps = {}));
+//# sourceMappingURL=saleOrderController.js.map
---
+++ Alps.Web/app/controllers/tradeAccountController.js
@@ -0,0 +1,124 @@
+var Alps;
+(function (Alps) {
+    var Controllers;
+    (function (Controllers) {
+        'use strict';
+        var TradeAccount = (function () {
+            function TradeAccount() {
+            }
+            return TradeAccount;
+        })();
+        Controllers.TradeAccount = TradeAccount;
+        var TradeAccountListCtrl = (function () {
+            function TradeAccountListCtrl(scope, http, toaster) {
+                function getTradeAccountList() {
+                    http.get("/api/TradeAccount").success(function (data) {
+                        scope.items = data;
+                        toaster.success("提示", "载入成功");
+                    }).error(function (data) {
+                        toaster.error("错误", data.Message);
+                    });
+                }
+                ;
+                scope.getTradeAccountList = getTradeAccountList;
+                getTradeAccountList();
+            }
+            TradeAccountListCtrl.$inject = ["$scope", "$http", "toaster"];
+            return TradeAccountListCtrl;
+        })();
+        Controllers.TradeAccountListCtrl = TradeAccountListCtrl;
+        var TradeAccountCreateCtrl = (function () {
+            function TradeAccountCreateCtrl(scope, http, toaster, locationService) {
+                function createTradeAccount() {
+                    http.post("/api/TradeAccount", scope.item).success(function (data) {
+                        toaster.success("提示", "创建成功");
+                        locationService.path("/TradeAccount");
+                    }).error(function (err) {
+                        var errMsg = "";
+                        if (err.ModelState) {
+                            errMsg = "<ul>";
+                            for (var p in err.ModelState) {
+                                errMsg = errMsg + "<li>" + err.ModelState[p][0] + "</li>";
+                            }
+                            errMsg = errMsg + "</ul>";
+                        }
+                        if (errMsg == "") {
+                            errMsg = err.Message;
+                        }
+                        toaster.pop("error", "错误", errMsg, 3000, "trustedHtml");
+                    });
+                }
+                ;
+                function goBack() {
+                    window.history.back();
+                }
+                scope.createTradeAccount = createTradeAccount;
+                scope.goBack = goBack;
+            }
+            TradeAccountCreateCtrl.$inject = ["$scope", "$http", "toaster", "$location"];
+            return TradeAccountCreateCtrl;
+        })();
+        Controllers.TradeAccountCreateCtrl = TradeAccountCreateCtrl;
+        var TradeAccountEditCtrl = (function () {
+            function TradeAccountEditCtrl(scope, http, toaster, locationService, routeParams, window) {
+                function getTradeAccount(id) {
+                    http.get("/api/TradeAccount/" + id).success(function (data) {
+                        scope.item = data;
+                        toaster.success("提示", "载入成功");
+                    }).error(function (data) {
+                        toaster.error("错误", data.Message);
+                    });
+                }
+                ;
+                function saveTradeAccount() {
+                    http.put("/api/TradeAccount/" + id, scope.item).success(function (data) {
+                        toaster.success("提示", "保存成功");
+                        locationService.path("/TradeAccount");
+                    }).error(function (err) {
+                        var errMsg = "";
+                        if (err.ModelState) {
+                            errMsg = "<ul>";
+                            for (var p in err.ModelState) {
+                                errMsg = errMsg + "<li>" + err.ModelState[p][0] + "</li>";
+                            }
+                            errMsg = errMsg + "</ul>";
+                        }
+                        if (errMsg == "") {
+                            errMsg = err.Message;
+                        }
+                        toaster.pop("error", "错误", errMsg, 3000, "trustedHtml");
+                    });
+                }
+                function deleteTradeAccount() {
+                    if (confirm("确认要删除?")) {
+                        http.delete("/api/TradeAccount/" + id).success(function (data) {
+                            toaster.success("提示", "删除成功");
+                            locationService.path("/TradeAccount");
+                        }).error(function (err) {
+                            toaster.error("错误", err.Message);
+                        });
+                    }
+                }
+                function goBack() {
+                    window.history.back();
+                }
+                scope.getTradeAccount = getTradeAccount;
+                scope.saveTradeAccount = saveTradeAccount;
+                scope.deleteTradeAccount = deleteTradeAccount;
+                scope.goBack = goBack;
+                var id = "";
+                if (routeParams["id"]) {
+                    id = routeParams["id"];
+                    getTradeAccount(id);
+                }
+                else {
+                    locationService.path("/TradeAccount");
+                }
+            }
+            TradeAccountEditCtrl.$inject = ["$scope", "$http", "toaster", "$location", "$routeParams", "$window"];
+            return TradeAccountEditCtrl;
+        })();
+        Controllers.TradeAccountEditCtrl = TradeAccountEditCtrl;
+    })(Controllers = Alps.Controllers || (Alps.Controllers = {}));
+})(Alps || (Alps = {}));
+//# sourceMappingURL=tradeAccountController.js.map
